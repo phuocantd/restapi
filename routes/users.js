baca02f86fdd2d8c99c870f273b64b57df37f5a6
phuocantd <phuocantd@gmail.com>
2019-10-16 21:41:08 +0700

    login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.get("/", (req, res, next) => {
         users: docs.map(doc => {
           return {
             email: doc.email,
-            password: doc.password,
+            password: doc.password
           };
         })
       };
@@ -47,54 +48,87 @@ router.get("/me", (req, res, next) => {
 
 router.post("/register", (req, res, next) => {
   User.find({ email: req.body.email })
-  .exec()
-  .then(user => {
-    if (user.length >= 1) {
-      return res.status(409).json({
-        message: "Mail exist"
-      });
-    } else {
-      bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          return res.status(500).json({
-            error: err
-          });
-        } else {
-          const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: req.body.email,
-            password: hash
-          });
-          user
-            .save()
-            .then(result => {
-              console.log(result);
-              res.status(201).json({
-                message: "User created"
-              });
-            })
-            .catch(err => {
-              console.log(err);
-              res.status(500).json({
-                error: err
-              });
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail exist"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
             });
-        }
-      });
-    }
-  });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created"
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    });
 });
 
 router.post("/login", (req, res, next) => {
-  const order = {
-    email: req.body.email,
-    password: req.body.password
-  };
-  console.log(order)
-  res.status(201).json({
-    message: "Order was created",
-    order
-  });
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Auth failed"
+            });
+          }
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: user[0].email,
+                userId: user[0]._id
+              },
+              "WakandaForever",
+              {
+                expiresIn: "1h"
+              }
+            );
+            return res.status(200).json({
+              message: "Auth successfull",
+              token: token
+            });
+          }
+          res.status(401).json({
+            message: "Auth failed"
+          });
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
