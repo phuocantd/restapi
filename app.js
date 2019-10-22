a85c78c87599d18d2cf423c4d21ddda85159b914
phuocantd <phuocantd@gmail.com>
2019-10-22 22:39:33 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const dotenv=require('dotenv')
+dotenv.config()
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const mongoose = require("mongoose");
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const passport = require("passport");
-
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "1612009" });
+  res.render("index", { title: '1612009' });
 });
 
 router.get(
